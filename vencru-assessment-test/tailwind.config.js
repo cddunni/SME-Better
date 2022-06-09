bf67b1b1d73e9c145ae4064ba323953e83504521
oluwatimileyin <oluwatimileyinakinpelu49@gmail.com>
2022-06-09 23:41:37 +0100

    added custom styles to tailwind config file

--- vencru-assessment-test/tailwind.config.js
+++ vencru-assessment-test/tailwind.config.js
@@ -3,7 +3,40 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'avenir': [ 'Avenir Next', 'sans-serif'],
+      'monteserrat': 'Montserrat, sans-serif',
+    },
+    letterSpacing: {
+      widest: '.21em'
+    },
+    inset: {
+      '12': '14px',
+      '17': '19px'
+    },
+    extend: {
+      colors: {
+        blue: {
+          '350': '#79A6F6',
+          '400': '#2D74DA'
+        },
+        gray: {
+          '120': 'rgba(0, 0, 0, 0.5)',
+          '150': '#E0E0E0'
+        },
+        black: {
+          '100': '#454749'
+        }
+      },
+      spacing: {
+        '7': '1.625rem',
+        '14': '3.75rem',
+        '15': '3.875rem',
+        '11': '2.875rem',
+        '17': '4.375rem',
+        '21': '5.625rem'    
+      }
+    },
   },
   plugins: [],
 }
